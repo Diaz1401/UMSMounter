06c83a0e17a32d03baaaaf1fc4df1c14eac3e502
kifixo <danielhuici@hotmail.com>
2023-02-27 22:55:20 +0100

    Fix root checker

--- app/src/main/java/com/jimzrt/umsmounter/tasks/CheckRootTask.kt
+++ app/src/main/java/com/jimzrt/umsmounter/tasks/CheckRootTask.kt
@@ -1,13 +1,16 @@
 package com.jimzrt.umsmounter.tasks
 
+import android.util.Log
 import com.topjohnwu.superuser.Shell
 
 class CheckRootTask : BaseTask() {
     override fun execute(): Boolean {
-        return if (Shell.rootAccess()) {
+        Shell.cmd().exec() // We need this so the next function works
+        return if (Shell.isAppGrantedRoot() == true) {
             result = "Root working!\n"
             true
         } else {
+            Log.d("Root", "Root status ${Shell.isAppGrantedRoot().toString()}")
             result = "Root not working!\n"
             false
         }
