e4d0d7efc255418e146af1d602fc9e0a0a96e49a
kifixo <danielhuici@hotmail.com>
2023-02-28 00:50:37 +0100

    Use SuFile to check files on root context

--- app/src/main/java/com/jimzrt/umsmounter/tasks/CheckMassStorageTask.kt
+++ app/src/main/java/com/jimzrt/umsmounter/tasks/CheckMassStorageTask.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.util.Log
 import com.topjohnwu.superuser.Shell
+import com.topjohnwu.superuser.io.SuFile
 import java.io.File
 
 const val CONFIGFS_DIR = "/config/usb_gadget/g1/functions/mass_storage.0/lun.0"
@@ -18,13 +19,13 @@ class CheckMassStorageTask : BaseTask() {
     override fun execute(): Boolean {
         val sharedPref = ctx!!.get()!!.getSharedPreferences(null, Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
-        return if (suPathExists(CONFIGFS_DIR)) {
+        return if (SuFile(CONFIGFS_DIR).exists()) {
             setConfigAsConfigFS(editor)
             true
-        } else if (suPathExists(ANDROIDUSB_DIR)) {
+        } else if (SuFile(ANDROIDUSB_DIR).exists()) {
             setConfigAsAndroidUsb(editor)
             true
-        } else if (suPathExists(ALT_ANDROIDUSB_DIR)) {
+        } else if (SuFile(ALT_ANDROIDUSB_DIR).exists()) {
             setConfigAsAltAndroidUsb(editor)
             true
         } else {
@@ -57,9 +58,7 @@ class CheckMassStorageTask : BaseTask() {
         editor.apply()
     }
     fun suPathExists(path: String): Boolean {
-        var result = Shell.cmd("if [ -d \"$path\" ]; then echo true; fi").exec()
-        Log.d("Result", "Resultado: ${result.out}")
-        return result.out.isNotEmpty()
+        return SuFile(path).exists()
     }
 
     init {
