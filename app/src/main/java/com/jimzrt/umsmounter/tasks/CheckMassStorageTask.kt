1c0746e8ef66dcf901417c7fc2c0e3d1e65926d2
kifixo <danielhuici@hotmail.com>
2023-02-27 22:45:05 +0100

    Refactor CheckMassStorageTask

--- app/src/main/java/com/jimzrt/umsmounter/tasks/CheckMassStorageTask.kt
+++ app/src/main/java/com/jimzrt/umsmounter/tasks/CheckMassStorageTask.kt
@@ -1,51 +1,31 @@
 package com.jimzrt.umsmounter.tasks
 
 import android.content.Context
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
+import android.util.Log
 import com.topjohnwu.superuser.Shell
 import java.io.File
 
+const val CONFIGFS_DIR = "/config/usb_gadget/g1/functions/mass_storage.0/lun.0"
+const val CONFIGFS_CDROM_DIR = "/config/usb_gadget/g1/functions/mass_storage.0/lun.0/cdrom"
+const val ANDROIDUSB_DIR = "/sys/class/android_usb/android0/f_mass_storage/lun"
+const val ANDROIDUSB_CDROM_DIR = "/sys/class/android_usb/android0/f_mass_storage/lun/cdrom"
+const val ALT_ANDROIDUSB_DIR = "/sys/class/android_usb/android0/f_mass_storage/lun0"
+const val ALT_ANDROIDUSB_CDROM_DIR = "/sys/class/android_usb/android0/f_mass_storage/lun"
+
 class CheckMassStorageTask : BaseTask() {
-    var CONFIGFS_DIR = "/config/usb_gadget/g1/functions/mass_storage.0/lun.0"
-    var CONFIGFS_CDROM_DIR = "/config/usb_gadget/g1/functions/mass_storage.0/lun.0/cdrom"
     override fun execute(): Boolean {
         val sharedPref = ctx!!.get()!!.getSharedPreferences(null, Context.MODE_PRIVATE)
         val editor = sharedPref.edit()
-        val usbDir = File("/sys/class/android_usb/android0/f_mass_storage/lun")
-        val usbDir2 = File("/sys/class/android_usb/android0/f_mass_storage/lun0")
         return if (suPathExists(CONFIGFS_DIR)) {
-            result = "mass_storage via configfs supported!\n"
-            editor.putString("usbMode", "configfs")
-            editor.putString("usbPath", CONFIGFS_DIR)
-            if (suPathExists(CONFIGFS_CDROM_DIR)) {
-                editor.putBoolean("cdrom", true)
-            } else {
-                editor.putBoolean("cdrom", false)
-            }
-            editor.apply()
+            setConfigAsConfigFS(editor)
             true
-        } else if (usbDir.exists() && usbDir.isDirectory) {
-            result = "mass_storage via android_usb supported!\n"
-            editor.putString("usbMode", "android_usb")
-            editor.putString("usbPath", usbDir.absolutePath)
-            val cdrom = File("/sys/class/android_usb/android0/f_mass_storage/lun/cdrom")
-            if (cdrom.exists()) {
-                editor.putBoolean("cdrom", true)
-            } else {
-                editor.putBoolean("cdrom", false)
-            }
-            editor.apply()
+        } else if (suPathExists(ANDROIDUSB_DIR)) {
+            setConfigAsAndroidUsb(editor)
             true
-        } else if (usbDir2.exists() && usbDir2.isDirectory) {
-            result = "mass_storage via android_usb supported!\n"
-            editor.putString("usbMode", "android_usb")
-            editor.putString("usbPath", usbDir2.absolutePath)
-            val cdrom = File("/sys/class/android_usb/android0/f_mass_storage/lun/cdrom")
-            if (cdrom.exists()) {
-                editor.putBoolean("cdrom", true)
-            } else {
-                editor.putBoolean("cdrom", false)
-            }
-            editor.apply()
+        } else if (suPathExists(ALT_ANDROIDUSB_DIR)) {
+            setConfigAsAltAndroidUsb(editor)
             true
         } else {
             result = "mass_storage not supported!\n"
@@ -53,10 +33,33 @@ class CheckMassStorageTask : BaseTask() {
         }
     }
 
+    fun setConfigAsConfigFS(editor: Editor) {
+        result = "mass_storage via configfs supported!\n"
+        editor.putString("usbMode", "configfs")
+        editor.putString("usbPath", CONFIGFS_DIR)
+        editor.putBoolean("cdrom", suPathExists(CONFIGFS_CDROM_DIR))
+        editor.apply()
+    }
 
+    fun setConfigAsAndroidUsb(editor: Editor) {
+        result = "mass_storage via android_usb supported!\n"
+        editor.putString("usbMode", "android_usb")
+        editor.putString("usbPath", ANDROIDUSB_DIR)
+        editor.putBoolean("cdrom", suPathExists(ANDROIDUSB_CDROM_DIR))
+        editor.apply()
+    }
+
+    fun setConfigAsAltAndroidUsb(editor: Editor) {
+        result = "mass_storage via android_usb supported!\n"
+        editor.putString("usbMode", "android_usb")
+        editor.putString("usbPath", ALT_ANDROIDUSB_DIR)
+        editor.putBoolean("cdrom", suPathExists(ALT_ANDROIDUSB_CDROM_DIR))
+        editor.apply()
+    }
     fun suPathExists(path: String): Boolean {
-        var result = Shell.cmd("if [ -d \"$path\" ]; echo true; fi").exec()
-        return result.out[0] == "true"
+        var result = Shell.cmd("if [ -d \"$path\" ]; then echo true; fi").exec()
+        Log.d("Result", "Resultado: ${result.out}")
+        return result.out.isNotEmpty()
     }
 
     init {
